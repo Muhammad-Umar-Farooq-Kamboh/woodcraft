b14e8837d9af2dba98405a37b4ffbcd4dc3e3ed2
Muhammad Umer Farooq <144126501+Muhammad-Umar-Farooq-Kamboh@users.noreply.github.com>
2026-04-30 18:25:51 +0500

    Update the skeleton on the page load of admin order

--- src/app/(app)/admin/orders/page.tsx
+++ src/app/(app)/admin/orders/page.tsx
@@ -1,36 +1,54 @@
 "use client";
 
 import OrderLIstForAdmin from "@/components/custom/order/admin/OrderLIstForAdmin";
+import OrderListSkeleton from "@/components/custom/order/admin/OrderListSkeleton";
+import { Skeleton } from "@/components/ui/skeleton";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Page() {
   const [listOfOrders, setListOfOrders] = useState([]);
   const [listOfEmployee, setListOfEmployee] = useState([]);
+  const [isPageLoading, setIsPageLoading] = useState(true);
   useEffect(() => {
     (async function () {
       const res = await axios.get("/api/order/get-all-orderlist");
       setListOfOrders(res.data.data);
       const resemp = await axios.get("/api/employee/get-all-employee-only");
       setListOfEmployee(resemp.data.data);
+      setIsPageLoading(false);
     })();
   }, []);
   return (
     <div className="px-5 py-7 w-full flex flex-col gap-8">
-      <div className="flex justify-between items-center">
-        <div>
-          <h3 className="text-2xl font-bold text-[#291D14]">Orders</h3>
-          <p className="text-[#745247] text-[14px]">
-            {listOfOrders.length} total orders
-          </p>
+      {isPageLoading ? (
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2 w-full">
+            <Skeleton className="h-[30px] w-[150px] rounded-sm bg-gray-300" />
+            <Skeleton className="h-[15px] w-[100px] rounded-sm bg-gray-300" />
+          </div>
+          <Skeleton className="h-[40px] w-[150px] rounded-sm bg-gray-300" />
         </div>
-        Add New Order
-      </div>
-      <OrderLIstForAdmin
-        data={listOfOrders}
-        employee={listOfEmployee}
-        setListOfOrders={setListOfOrders}
-      />
+      ) : (
+        <div className="flex justify-between items-center">
+          <div>
+            <h3 className="text-2xl font-bold text-[#291D14]">Orders</h3>
+            <p className="text-[#745247] text-[14px]">
+              {listOfOrders.length} total orders
+            </p>
+          </div>
+          Add New Order
+        </div>
+      )}
+      {isPageLoading ? (
+        <OrderListSkeleton />
+      ) : (
+        <OrderLIstForAdmin
+          data={listOfOrders}
+          employee={listOfEmployee}
+          setListOfOrders={setListOfOrders}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/custom/order/admin/OrderListSkeleton.tsx
@@ -0,0 +1,127 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+export default function OrderListSkeleton() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="grid gap-5 md:grid-cols-8">
+        <Skeleton className="h-[40px] w-full rounded-sm bg-gray-300 col-span-3" />
+        <Skeleton className="h-[40px] w-[100px] rounded-sm bg-gray-300" />
+        <Skeleton className="h-[40px] w-[100px] rounded-sm bg-gray-300" />
+        <Skeleton className="h-[40px] w-[100px] rounded-sm bg-gray-300" />
+        <Skeleton className="h-[40px] w-[100px] rounded-sm bg-gray-300" />
+        <Skeleton className="h-[40px] w-[100px] rounded-sm bg-gray-300" />
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 border-1 p-5 rounded-2xl">
+          <div className="flex justify-around">
+            <div className="flex flex-col w-full gap-2">
+              <Skeleton className="h-[20px] w-[250px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[10px] w-[400px] rounded-sm bg-gray-300" />
+            </div>
+            <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-y-2">
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 border-1 p-5 rounded-2xl">
+          <div className="flex justify-around">
+            <div className="flex flex-col w-full gap-2">
+              <Skeleton className="h-[20px] w-[250px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[10px] w-[400px] rounded-sm bg-gray-300" />
+            </div>
+            <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-y-2">
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 border-1 p-5 rounded-2xl">
+          <div className="flex justify-around">
+            <div className="flex flex-col w-full gap-2">
+              <Skeleton className="h-[20px] w-[250px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[10px] w-[400px] rounded-sm bg-gray-300" />
+            </div>
+            <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-y-2">
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 border-1 p-5 rounded-2xl">
+          <div className="flex justify-around">
+            <div className="flex flex-col w-full gap-2">
+              <Skeleton className="h-[20px] w-[250px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[10px] w-[400px] rounded-sm bg-gray-300" />
+            </div>
+            <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-y-2">
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+              <Skeleton className="h-[20px] w-[80px] rounded-sm bg-gray-300" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
